f0812c79ddb956fbf194a4acb05e5f93f8a205c1
Leonardo Ledda <leonardoledda@gmail.com>
2023-09-29 22:46:03 +0200

    extra: Inherit ih8sn makefile
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- product.mk
+++ product.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit ih8sn Makefile
+$(call inherit-product, vendor/extra/external/ih8sn/product.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
