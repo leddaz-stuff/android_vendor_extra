7b33efa11ca89b088e8aa543d3abccbeff2c648d
Leonardo Ledda <leonardoledda@gmail.com>
2023-09-29 22:48:54 +0200

    extra: Conditionally inherit MindTheGapps makefile
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

--- product.mk
+++ product.mk
@@ -7,6 +7,15 @@
 # Inherit ih8sn Makefile
 $(call inherit-product, vendor/extra/external/ih8sn/product.mk)
 
+# MindTheGapps
+ifeq ($(INCLUDE_GAPPS), true)
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+else
+$(call inherit-product-if-exists, vendor/gapps/arm/arm-vendor.mk)
+endif
+endif
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
